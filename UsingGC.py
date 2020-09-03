ca95f10827e96944bd6955ac84c830284aa6d02f
grululu <70691214+grululu@users.noreply.github.com>
2020-09-03 12:21:32 +0200

    Update UsingGC.py

--- UsingGC.py
+++ UsingGC.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
 from GCExtract import GarminConnect as GC
-from dbSetup import con, meta, get_garmin_id
+#from dbSetup import con, meta, get_garmin_id
 from ImportActivities import gpx2pg, csv2pg
 from Infos import GCuser, GCpass
 from selenium.webdriver.chrome.options import Options
+
+def get_garmin_id():
+        """Get from database the activities ids already saved"""
+        return {}
+      
 
 # creating display
 display = Display(visible = 0, size = (1080, 1920))
@@ -15,7 +20,7 @@ options.add_argument('--no-sandbox')
 chrome = webdriver.Chrome(chrome_options=options,service_args=["--verbose", "--log-path=/tmp/chrome.log"])
 GC = GC(chrome)
 GC.login(userName = GCuser, passWord = GCpass)
-saved_ids = get_garmin_id(con)
+saved_ids = get_garmin_id()
 GC.getActivities(saved_ids)
 
 # importing Activities to Database
