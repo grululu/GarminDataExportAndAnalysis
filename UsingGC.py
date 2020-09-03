e4ceb2132f6085305400dd0ab632797a7f08a48b
grululu <70691214+grululu@users.noreply.github.com>
2020-09-03 18:26:46 +0200

    Update UsingGC.py

--- UsingGC.py
+++ UsingGC.py
@@ -24,6 +24,7 @@ options.add_arguments("test-type")
 options.add_arguments("--js-flags=--expose-gc")
 options.add_arguments("--enable-precise-memory-info")
 options.add_arguments("--disable-default-apps")
+options.add_arguments("--start-maximized")
 
 chrome = webdriver.Chrome(chrome_options=options,service_args=["--verbose", "--log-path=/tmp/chrome.log"])
 GC = GC(chrome)
