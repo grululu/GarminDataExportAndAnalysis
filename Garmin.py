f9e141c6df2b03d392502bc3d4162412dbb4e1a0
grululu <70691214+grululu@users.noreply.github.com>
2020-09-04 18:07:40 +0200

    Update and rename UsingGC.py to Garmin.py

---
+++ Garmin.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from pyvirtualdisplay import Display
+from GCDriver import GarminConnect as GC
+from Infos import GCuser, GCpass
+from selenium.webdriver.chrome.options import Options
+
+display = Display()
+display.start()
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument("--disable-dev-shm-usage")
+options.add_experimental_option("prefs", {"download.default_directory": "/path/to/download/dir","download.prompt_for_download": False,})
+
+chrome = webdriver.Chrome(chrome_options=options,service_args=["--verbose", "--log-path=/tmp/chrome.log"])
+GC = GC(chrome)
+GC.login(userName = GCuser, passWord = GCpass)
+GC.getActivities()
+GC.close()
+
