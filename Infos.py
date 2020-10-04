b341a9a0a5bfd69e921f887f5ec77b859ef16a99
grululu <70691214+grululu@users.noreply.github.com>
2020-10-04 16:29:18 +0200

    Delete Infos.py

--- Infos.py
+++
@@ -1,6 +0,0 @@
-GCuser="YOUR_USER_NAME" # Garmin Connect User Name
-GCpass="YOUR_PASSWORD" # Garmin Connect Password
-databaseServer = "database_host" # Database Serve adress
-databaseName = "dbName" # Database Name
-databaseUser = "usrName" # Database User Name
-databasePW = "usrPassWord" # Database Password
