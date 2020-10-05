67b5aca0e9566e26d1d12f69abb49685bf2cce86
grululu <umberto.caselli@gmail.com>
2020-10-05 16:38:51 +0200

    commit

--- MyApp/run.sh
+++ MyApp/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #git clone https://github.com/grululu/GarminDataExportAndAnalysis
 cd /MyApp
+export GARMIN_USER=$(echo $GARMIN_USER | tr -d '\040\011\012\015')
+export GARMIN_PASSWORD=$(echo $GARMIN_PASSWORD | tr -d '\040\011\012\015')
+
 echo "GCuser=\"$GARMIN_USER\"" > Infos.py
 echo "GCpass=\"$GARMIN_PASSWORD\"" >> Infos.py
 
-cp /Credentials/* . 
 python3 Garmin.py
-python3 loadActivitiesOnGoogleDrive.py
-python3 triggerUpdate.py
+python3 loadActivitiesOnGo
