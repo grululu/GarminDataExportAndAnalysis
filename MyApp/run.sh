5373a6d9bd8abbdf124bc0318ecd9f750b9230cc
grululu <70691214+grululu@users.noreply.github.com>
2020-10-06 16:09:33 +0200

    Update run.sh

--- MyApp/run.sh
+++ MyApp/run.sh
@@ -7,6 +7,6 @@ export GARMIN_PASSWORD=$(echo $GARMIN_PASSWORD | tr -d '\040\011\012\015')
 echo "GCuser=\"$GARMIN_USER\"" > Infos.py
 echo "GCpass=\"$GARMIN_PASSWORD\"" >> Infos.py
 
-python3 Garmin.py
+python3 downloadActivities.py
 python3 loadActivitiesOnGoogleDrive.py
 python3 triggerUpdate.py
