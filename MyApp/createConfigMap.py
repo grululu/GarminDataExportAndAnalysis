f20afd59f3da9839e2e74efe483a1202420460c4
grululu <70691214+grululu@users.noreply.github.com>
2020-10-10 18:18:19 +0200

    Create createConfigMap.py

---
+++ MyApp/createConfigMap.py
@@ -0,0 +1,50 @@
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+from pprint import pprint
+
+
+def create_configmap(api_instance, configmap):
+    try:
+        api_response = api_instance.create_namespaced_config_map(
+            namespace="default",
+            body=configmap,
+            pretty = 'pretty_example',
+        )
+        pprint(api_response)
+
+    except ApiException as e:
+        print("Exception when calling CoreV1Api->create_namespaced_config_map: %s\n" % e)
+
+
+def create_configmap_object():
+    # Configureate ConfigMap metadata
+    metadata = client.V1ObjectMeta(
+        annotations=dict(app="test", person="chummy"),
+        deletion_grace_period_seconds=30,
+        labels=dict(app="test", person="chummy"),
+        name="Activities",
+        namespace="default",
+    )
+    # Get File Content
+    with open('/MyApp/Activities.csv', 'r') as f:
+        file_content=f.read()
+    # Instantiate the configmap object
+    configmap = client.V1ConfigMap(
+        api_version="v1",
+        kind="ConfigMap",
+        data=dict(test=file_content),
+        metadata=metadata
+    )
+
+    return configmap
+
+def main():
+    #config.load_kube_config()
+    configuration = client.Configuration()
+    api_instance = client.CoreV1Api(client.ApiClient(configuration))
+
+    configmap = create_configmap_object()
+    create_configmap(api_instance, configmap)
+
+if __name__ == '__main__':
+    main()
