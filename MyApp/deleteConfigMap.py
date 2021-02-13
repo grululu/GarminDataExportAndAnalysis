6efd2b15203f985bae9cd0efb9d49b3b3b6c151b
grululu <70691214+grululu@users.noreply.github.com>
2021-02-13 19:24:50 +0100

    Update deleteConfigMap.py

--- MyApp/deleteConfigMap.py
+++ MyApp/deleteConfigMap.py
@@ -3,19 +3,6 @@ from kubernetes.client.rest import ApiException
 from pprint import pprint
 
 
-def create_configmap(api_instance, configmap):
-    try:
-        api_response = api_instance.create_namespaced_config_map(
-            namespace="tws95",
-            body=configmap,
-            pretty = 'pretty_example',
-        )
-        pprint(api_response)
-
-    except ApiException as e:
-        print("Exception when calling CoreV1Api->create_namespaced_config_map: %s\n" % e)
-
-
 def deleteConfigMap(api_instance,namespace,name):
         print("delete config map")
         body = client.V1DeleteOptions()  # V1DeleteOptions |
@@ -27,33 +14,11 @@ def deleteConfigMap(api_instance,namespace,name):
         except ApiException as e:
           print("Exception when calling CoreV1Api->delete_namespaced_config_map: %s\n" % e)
 
-def create_configmap_object():
-    # Configureate ConfigMap metadata
-    metadata = client.V1ObjectMeta(
-        annotations=dict(app="test", person="chummy"),
-        deletion_grace_period_seconds=30,
-        labels=dict(app="test", person="chummy"),
-        name="activities",
-        namespace="tws95",
-    )
-    # Get File Content
-    with open('/MyApp/Activities.csv', 'r') as f:
-        file_content=f.read()
-    # Instantiate the configmap object
-    configmap = client.V1ConfigMap(
-        api_version="v1",
-        kind="ConfigMap",
-        data=dict(activities=file_content),
-        metadata=metadata
-    )
-
-    return configmap
-
 def main():
     config.load_kube_config()
     configuration = client.Configuration()
-    api_instance = client.CoreV1Api(client.ApiClient(configuration))
-    deleteConfigMap(api_instance,"tws95","activities")
+    api_instance = client.CoreV1Api()
+    deleteConfigMap(api_instance,"umbertorace","activities")
 
 if __name__ == '__main__':
     main()
