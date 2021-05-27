86f903447bbe03a2520b7bbaed71d7549dc816b4
grululu <70691214+grululu@users.noreply.github.com>
2021-05-27 19:45:28 +0200

    Update downloadActivities.py

--- MyApp/downloadActivities.py
+++ MyApp/downloadActivities.py
@@ -10,32 +10,34 @@ import time
 class GarminConnect:
     def __init__(self, driver):
         self.driver = driver
-        self.urlLogin = 'https://sso.garmin.com/sso/login?service=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&webhost=olaxpw-connect04&source=https%3A%2F%2Fconnect.garmin.com%2Fen-US%2Fsignin&redirectAfterAccountLoginUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&redirectAfterAccountCreationUrl=https%3A%2F%2Fconnect.garmin.com%2Fpost-auth%2Flogin&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso&locale=en_US&id=gauth-widget&cssUrl=https%3A%2F%2Fstatic.garmincdn.com%2Fcom.garmin.connect%2Fui%2Fcss%2Fgauth-custom-v1.1-min.css&clientId=GarminConnect&rememberMeShown=true&rememberMeChecked=false&createAccountShown=true&openCreateAccount=false&usernameShown=false&displayNameShown=false&consumeServiceTicket=false&initialFocus=true&embedWidget=false&generateExtraServiceTicket=false'
         self.urlActivities = 'https://connect.garmin.com/modern/activities'
         self.driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
         params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': "/MyApp"}}
         command_result = self.driver.execute("send_command", params)
 
-    def login(self, userName, passWord):
-        """Function to login to Garmin Connect page"""
-        self.driver.get(self.urlLogin)
-        assert "GARMIN Authentication Application" in self.driver.title
-        self.driver.find_element_by_id("username").send_keys(userName)
-        self.driver.find_element_by_id("password").send_keys(passWord)
-        self.driver.find_element_by_id("login-btn-signin").click()
-        print("Logged in!")
-        return self.driver
-
-
-    def getActivities(self):
+    def getActivities(self,userName,passWord):
         """Function to navigate on activities page and download data"""
         self.driver.get(self.urlActivities)
         assert "Garmin Connect" in self.driver.title
+        self.driver.save_screenshot("/tmp/activities.png")
         print("Getting Activities")
         self.driver.set_window_size(1920, 1080)
+        self.driver.save_screenshot("/tmp/activities1.png")
+        frameLogin= self.driver.find_element_by_id("gauth-widget-frame-gauth-widget")
+        self.driver.switch_to.frame(frameLogin)
+        self.driver.find_element_by_id("username").send_keys(userName)
+        self.driver.find_element_by_id("password").send_keys(passWord)
+        print("Username "+userName)
+        self.driver.find_element_by_id("login-btn-signin").click()
 
-        WebDriverWait(self.driver, 120).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "export-btn"))).click()
+
+        try:
+            WebDriverWait(self.driver, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "export-btn"))).click()
+        except Exception as err:
+            exception_type = type(err).__name__
+            print(exception_type)
+            self.driver.save_screenshot("/tmp/activities2.png")
         print("Download Complete")
 
     def close(self):
@@ -51,7 +53,5 @@ options.add_experimental_option("prefs", {"download.default_directory": "/path/t
 
 chrome = webdriver.Chrome(chrome_options=options,service_args=["--verbose", "--log-path=/tmp/chrome.log"])
 GC = GarminConnect(chrome)
-GC.login(userName = GCuser, passWord = GCpass)
-GC.getActivities()
+GC.getActivities(userName = GCuser, passWord = GCpass)
 GC.close()
-
