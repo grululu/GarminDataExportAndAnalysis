6e676536ccab3b3e2d5f8e8ba6798b333f0befdf
grululu <70691214+grululu@users.noreply.github.com>
2023-08-26 17:53:59 +0200

    Update run.sh

--- MyApp/run.sh
+++ MyApp/run.sh
@@ -7,13 +7,14 @@ export GARMIN_PASSWORD=$(echo $GARMIN_PASSWORD | tr -d '\040\011\012\015')
 echo "GCuser=\"$GARMIN_USER\"" > Infos.py
 echo "GCpass=\"$GARMIN_PASSWORD\"" >> Infos.py
 
-python3 downloadActivities.py
+#python3 downloadActivities.py
+python3 downloadActivitiesWithGarminConnect.py
 if test -f /MyApp/Activities.csv ; 
 then echo "Download Completed" ; 
 else
 echo "DOWNLOAD FAILED, TRYING AGAIN" ; 
 sleep 6000
-python3 downloadActivities.py
+python3 downloadActivitiesWithGarminConnect.py
 fi
 
 if test -f /MyApp/Activities.csv ; 
